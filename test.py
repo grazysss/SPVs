b4e3f959432f97b61feba8ab599d4e8336f343b5
Grazielly Souza <graziellysouza2580@gmail.com>
2025-07-30 16:12:52 -0300

    first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+from sistema_voos import *
+
+# -------------------------------------------------
+# 11) Bloco de teste                             🡇
+# -------------------------------------------------
+if __name__ == "__main__":
+    """
+    TODO:
+      • Criar 2 companhias, 2 voos cada, passageiros, funcionários e auditor.
+      • Adicionar bagagens, listar passageiros, auditar voos.
+      • Mostrar saídas no console para validar implementações.
+    """
+    pass
+
